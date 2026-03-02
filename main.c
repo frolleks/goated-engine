86227b7422954e354c3a9bd1e1dea28e290089dc
frolleks <spaceactive.akbar@gmail.com>
2026-03-02 19:23:51 +0700

    Initial commit

---
+++ main.c
@@ -0,0 +1,40 @@
+#define SDL_MAIN_USE_CALLBACKS
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3/SDL_init.h>
+
+SDL_Window *window;
+SDL_GPUDevice *device;
+
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv)
+{
+    // create a window
+    window = SDL_CreateWindow("Hello, Triangle!", 960, 540, SDL_WINDOW_RESIZABLE);
+
+    device = SDL_CreateGPUDevice(SDL_GPU_SHADERFORMAT_DXIL | SDL_GPU_SHADERFORMAT_SPIRV, true, NULL);
+
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+{
+    // close the window on request
+    if (event->type == SDL_EVENT_WINDOW_CLOSE_REQUESTED)
+    {
+        return SDL_APP_SUCCESS;
+    }
+
+    return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void *appstate, SDL_AppResult result)
+{
+    SDL_DestroyGPUDevice(device);
+    // destroy the window
+    SDL_DestroyWindow(window);
+}
