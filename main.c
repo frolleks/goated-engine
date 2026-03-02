4b58339aed046e0c3b4081f03681687180742638
frolleks <spaceactive.akbar@gmail.com>
2026-03-02 21:26:46 +0700

    Render white background

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 #include <SDL3/SDL_init.h>
+#include <SDL3/SDL_gpu.h>
 
 SDL_Window *window;
 SDL_GPUDevice *device;
@@ -19,6 +20,30 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv)
 
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
+    SDL_GPUCommandBuffer *command_buffer = SDL_AcquireGPUCommandBuffer(device);
+
+    SDL_GPUTexture *swapchain_texture;
+    uint32_t width, height;
+    SDL_WaitAndAcquireGPUSwapchainTexture(command_buffer, window, &swapchain_texture, &width, &height);
+
+    if (swapchain_texture == NULL)
+    {
+        SDL_SubmitGPUCommandBuffer(command_buffer);
+        return SDL_APP_CONTINUE;
+    }
+
+    SDL_GPUColorTargetInfo color_target_info = {0};
+
+    color_target_info.clear_color = (SDL_FColor){255 / 255.0f, 255 / 255.0f, 255 / 255.0f, 255 / 255.0f};
+    color_target_info.load_op = SDL_GPU_LOADOP_CLEAR;
+    color_target_info.store_op = SDL_GPU_STOREOP_STORE;
+    color_target_info.texture = swapchain_texture;
+
+    SDL_GPURenderPass *render_pass = SDL_BeginGPURenderPass(command_buffer, &color_target_info, 1, NULL);
+
+    SDL_EndGPURenderPass(render_pass);
+    SDL_SubmitGPUCommandBuffer(command_buffer);
+
     return SDL_APP_CONTINUE;
 }
 
