c2c10fa9d4f4c837c48e827f39ffafceeef8935b
frolleks <spaceactive.akbar@gmail.com>
2026-03-07 12:04:59 +0700

    Properly define the cube mesh

--- cube.h
+++ cube.h
@@ -1,28 +1,8 @@
+#ifndef PENGER_H
+#define PENGER_H
+
 #include "mesh.h"
 
-static const Vertex object_vertices[] = {
-    {0.25f, 0.25f, 0.25f},
-    {-0.25f, 0.25f, 0.25f},
-    {-0.25f, -0.25f, 0.25f},
-    {0.25f, -0.25f, 0.25f},
-    {0.25f, 0.25f, -0.25f},
-    {-0.25f, 0.25f, -0.25f},
-    {-0.25f, -0.25f, -0.25f},
-    {0.25f, -0.25f, -0.25f},
-};
+extern const Mesh cube_mesh;
 
-static const Triangle object_triangles[] = {
-    {0, 1, 2}, {0, 2, 3},
-    {4, 7, 6}, {4, 6, 5},
-    {0, 3, 7}, {0, 7, 4},
-    {1, 5, 6}, {1, 6, 2},
-    {0, 4, 5}, {0, 5, 1},
-    {3, 2, 6}, {3, 6, 7},
-};
-
-const Mesh object_mesh = {
-    .vertices = object_vertices,
-    .vertex_count = sizeof(object_vertices) / sizeof(object_vertices[0]),
-    .triangles = object_triangles,
-    .triangle_count = sizeof(object_triangles) / sizeof(object_triangles[0]),
-};
+#endif
