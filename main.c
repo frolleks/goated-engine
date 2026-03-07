3e5b9ef9f4a2afc7fb1cfc5676c39002d32a83db
frolleks <spaceactive.akbar@gmail.com>
2026-03-07 07:04:45 +0700

    Render a simple triangle

--- main.c
+++ main.c
@@ -1,56 +1,102 @@
-#define SDL_MAIN_USE_CALLBACKS
+#define SDL_MAIN_USE_CALLBACKS 1
+
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
-#include <SDL3/SDL_init.h>
-#include <SDL3/SDL_gpu.h>
 
-SDL_Window *window;
-SDL_GPUDevice *device;
+static SDL_Window *window = NULL;
+static SDL_Renderer *renderer = NULL;
 
+SDL_FPoint normalize_coords(float x, float y, int width, int height)
+{
+    float scale = SDL_min(width, height) * 0.5f;
+
+    const float half_width = width * 0.5f;
+    const float half_height = height * 0.5f;
+
+    return (SDL_FPoint){
+        half_width + (x * scale),
+        half_height - (y * scale),
+    };
+}
+
+/* Called once when SDL starts: set up the video system, window, and renderer. */
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv)
 {
-    // create a window
-    window = SDL_CreateWindow("Hello, Triangle!", 960, 540, SDL_WINDOW_RESIZABLE);
+    (void)appstate;
+    (void)argc;
+    (void)argv;
 
-    device = SDL_CreateGPUDevice(SDL_GPU_SHADERFORMAT_DXIL | SDL_GPU_SHADERFORMAT_SPIRV, true, NULL);
-    SDL_ClaimWindowForGPUDevice(device, window);
+    SDL_SetAppMetadata("Hello Triangle", "1.0", "com.frolleks.sdl-learning");
 
-    return SDL_APP_CONTINUE;
-}
-
-SDL_AppResult SDL_AppIterate(void *appstate)
-{
-    SDL_GPUCommandBuffer *command_buffer = SDL_AcquireGPUCommandBuffer(device);
-
-    SDL_GPUTexture *swapchain_texture;
-    uint32_t width, height;
-    SDL_WaitAndAcquireGPUSwapchainTexture(command_buffer, window, &swapchain_texture, &width, &height);
-
-    if (swapchain_texture == NULL)
+    if (!SDL_Init(SDL_INIT_VIDEO))
     {
-        SDL_SubmitGPUCommandBuffer(command_buffer);
-        return SDL_APP_CONTINUE;
+        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
     }
 
-    SDL_GPUColorTargetInfo color_target_info = {0};
-
-    color_target_info.clear_color = (SDL_FColor){255 / 255.0f, 255 / 255.0f, 255 / 255.0f, 255 / 255.0f};
-    color_target_info.load_op = SDL_GPU_LOADOP_CLEAR;
-    color_target_info.store_op = SDL_GPU_STOREOP_STORE;
-    color_target_info.texture = swapchain_texture;
-
-    SDL_GPURenderPass *render_pass = SDL_BeginGPURenderPass(command_buffer, &color_target_info, 1, NULL);
-
-    SDL_EndGPURenderPass(render_pass);
-    SDL_SubmitGPUCommandBuffer(command_buffer);
+    if (!SDL_CreateWindowAndRenderer("Hello, Triangle!", 960, 540, SDL_WINDOW_RESIZABLE, &window, &renderer))
+    {
+        SDL_Log("Couldn't create the window and renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
 
     return SDL_APP_CONTINUE;
 }
 
+/* Called every frame: build the triangle, clear the screen, draw, then present. */
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+    int width = 0;
+    int height = 0;
+    const SDL_FColor black = {0.0f, 0.0f, 0.0f, 1.0f};
+    SDL_Vertex triangle[3];
+
+    (void)appstate;
+
+    if (!SDL_GetRenderOutputSize(renderer, &width, &height))
+    {
+        SDL_Log("Couldn't get renderer size: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    /* Vertices are in centered normalized space: (0, 0) is screen center. */
+    triangle[0] = (SDL_Vertex){normalize_coords(0.0f, 0.0f, width, height), black, {0.0f, 0.0f}};
+    triangle[1] = (SDL_Vertex){normalize_coords(0.25f, -0.5f, width, height), black, {0.0f, 0.0f}};
+    triangle[2] = (SDL_Vertex){normalize_coords(-0.25f, -0.5f, width, height), black, {0.0f, 0.0f}};
+
+    if (!SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE))
+    {
+        SDL_Log("Couldn't set the clear color: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    if (!SDL_RenderClear(renderer))
+    {
+        SDL_Log("Couldn't clear the frame: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    if (!SDL_RenderGeometry(renderer, NULL, triangle, 3, NULL, 0))
+    {
+        SDL_Log("Couldn't draw the triangle: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    if (!SDL_RenderPresent(renderer))
+    {
+        SDL_Log("Couldn't present the frame: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    return SDL_APP_CONTINUE;
+}
+
+/* Called for incoming OS events. We only care about closing the window for now. */
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 {
-    // close the window on request
-    if (event->type == SDL_EVENT_WINDOW_CLOSE_REQUESTED)
+    (void)appstate;
+
+    if (event->type == SDL_EVENT_QUIT || event->type == SDL_EVENT_WINDOW_CLOSE_REQUESTED)
     {
         return SDL_APP_SUCCESS;
     }
@@ -58,9 +104,13 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
     return SDL_APP_CONTINUE;
 }
 
+/* Called once on exit: release SDL objects in the opposite order they were created. */
 void SDL_AppQuit(void *appstate, SDL_AppResult result)
 {
-    SDL_DestroyGPUDevice(device);
-    // destroy the window
+    (void)appstate;
+    (void)result;
+
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+    SDL_Quit();
 }
