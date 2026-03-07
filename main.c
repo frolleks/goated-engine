f39c5a1acd4308d206a0c9c158d7e44bc5376510
frolleks <spaceactive.akbar@gmail.com>
2026-03-07 11:44:01 +0700

    Simplify rendering pipeline code

--- main.c
+++ main.c
@@ -45,13 +45,21 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
         (float)(now - previous_counter) / (float)SDL_GetPerformanceFrequency();
     int width = 0;
     int height = 0;
-    SDL_FPoint projected_vertices[SDL_arraysize(object_vertices)];
     const float rotation_speed = 2.0f;
     const float camera_distance = 1.5f;
     const float point_size = 10.0f;
     angle += rotation_speed * dt;
     (void)appstate;
     previous_counter = now;
+
+    const Mesh *mesh = &penger_mesh;
+
+    SDL_FPoint *projected_vertices =
+        SDL_malloc(sizeof(*projected_vertices) * mesh->vertex_count);
+    if (!projected_vertices) {
+        SDL_Log("Out of memory");
+        return SDL_APP_FAILURE;
+    }
 
     if (!SDL_GetRenderOutputSize(renderer, &width, &height)) {
         SDL_Log("Couldn't get renderer size: %s", SDL_GetError());
@@ -86,22 +94,17 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
         return SDL_APP_FAILURE;
     }
 
-    for (int i = 0; i < (int)SDL_arraysize(object_vertices); ++i) {
-        Vertex rotated_coords = rotate_y(object_vertices[i], angle);
+    for (size_t i = 0; i < mesh->vertex_count; ++i) {
+        Vertex rotated = rotate_y(mesh->vertices[i], angle);
+        rotated.z += camera_distance;
 
-        rotated_coords.z += camera_distance;
+        if (rotated.z <= 0.01f) {
+            projected_vertices[i] = (SDL_FPoint){0.0f, 0.0f};
+            continue;
+        }
 
-        SDL_FPoint first_vs =
-            project_to_2d(rotated_coords.x, rotated_coords.y, rotated_coords.z);
-        SDL_FPoint rect_pos =
-            normalize_coords(first_vs.x, first_vs.y, width, height);
-        projected_vertices[i] = rect_pos;
-        const SDL_FRect rect = {
-            rect_pos.x - (point_size * 0.5f),
-            rect_pos.y - (point_size * 0.5f),
-            point_size,
-            point_size,
-        };
+        SDL_FPoint p = project_to_2d(rotated.x, rotated.y, rotated.z);
+        projected_vertices[i] = normalize_coords(p.x, p.y, width, height);
 
         // if (!SDL_RenderFillRect(renderer, &rect)) {
         //     SDL_Log("Couldn't draw the rectangle: %s", SDL_GetError());
@@ -109,13 +112,15 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
         // }
     }
 
-    for (int i = 0; i < (int)SDL_arraysize(object_triangles); ++i) {
-        const Triangle triangle = object_triangles[i];
-        const SDL_FPoint a = projected_vertices[triangle.v1];
-        const SDL_FPoint b = projected_vertices[triangle.v2];
-        const SDL_FPoint c = projected_vertices[triangle.v3];
+    for (size_t i = 0; i < mesh->triangle_count; ++i) {
+        Triangle t = mesh->triangles[i];
+
+        SDL_FPoint a = projected_vertices[t.v1];
+        SDL_FPoint b = projected_vertices[t.v2];
+        SDL_FPoint c = projected_vertices[t.v3];
 
         if (!render_triangle_outline(renderer, a, b, c)) {
+            SDL_free(projected_vertices);
             SDL_Log("Couldn't draw the triangle outline: %s", SDL_GetError());
             return SDL_APP_FAILURE;
         }
@@ -125,6 +130,8 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
         SDL_Log("Couldn't present the frame: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
+
+    SDL_free(projected_vertices);
 
     return SDL_APP_CONTINUE;
 }
